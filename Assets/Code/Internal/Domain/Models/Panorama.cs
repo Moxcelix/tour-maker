a7d8c52ba075392cfc4fac3258b21a215cc12dbf
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2025-12-02 14:45:10 +0300

    Update Panorama.cs

--- Assets/Code/Internal/Domain/Models/Panorama.cs
+++ Assets/Code/Internal/Domain/Models/Panorama.cs
@@ -1,10 +1,15 @@
 public class Panorama
 {
     public string Id { get; }
-
+    public string Name { get; private set; }
 
     public Panorama(string id)
     {
         Id = id;
+    }
+
+    public void Rename(string name)
+    {
+        Name = name;
     }
 }
